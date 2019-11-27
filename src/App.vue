a0226ae9bf0e49d4aaa5747143c7a9dcba5a977c
cuilei192 <876516308@qq.com>
2019-11-27 22:54:41 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+ <div id="app">
+    <mt-button @click.native="HandleClick">按钮</mt-button>
+   <mt-tabbar v-model="selected">
+     <mt-tab-item id="外卖">
+       <img slot="icon" src="./assets/logo.png">
+       外卖
+     </mt-tab-item>
+     <mt-tab-item id="订单">
+       <img slot="icon" src="./assets/logo.png">
+       订单
+     </mt-tab-item>
+     <mt-tab-item id="发现">
+       <img slot="icon" src="./assets/logo.png">
+       发现
+     </mt-tab-item>
+     <mt-tab-item id="我的">
+       <img slot="icon" src="./assets/logo.png">
+       我的
+     </mt-tab-item>
+   </mt-tabbar>
+  </div>
+</template>
+
+<script>
+export default {
+    el: 'App',
+    methods: {
+      HandleClick: function () {
+        this.$toast('Hello world');
+      }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
