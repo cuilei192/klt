674c57097c30a5095902f7e357dd0036e03f7e60
studymani <624921088@qq.com>
2019-12-05 00:26:39 +0800

    Update HeadBar.vue

--- src/components/HeadBar.vue
+++ src/components/HeadBar.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="middleView" style="height: 1.016rem;z-index: 1;background: #FFFFFF">
-    <div class="middleViewAS" style="width: 14%"> <img class="middleView" src="../../static/image/public/klt_bak_arrow.png" style="width: 18%"/></div>
-    <div class="middleViewA" style="width: 60%;font-size: 0.375rem">商品分类</div>
-    <div class="middleViewAE" style="width: 14%">
-      <img class="middleView" src="../../static/image/public/kefu.png" style="width: 30%"/>
+    <div class="middleViewAS" style="width: 15%">
+      <img class="middleView" src="../../static/image/public/klt_bak_arrow.png" style="width: 0.25rem;height: 0.422rem;margin-left: 0.422rem;"/>
+    </div>
+    <div class="middleViewA" style="width: 70%;">
+      <span style="font-size: 0.375rem">商品分类</span>
+    </div>
+    <div class="middleViewAE" style="width: 15%">
+      <img src="../../static/image/public/kefu.png" style="width: 0.406rem;height: 0.391rem;margin-right: 0.422rem;"/>
     </div>
   </div>
 </template>
