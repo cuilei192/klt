cbf63e819233df5856d12f6d9cde85b8a53808ba
cuilei192 <876516308@qq.com>
2019-12-05 00:28:31 +0800

    1

--- src/pages/KltPageOrder.vue
+++ src/pages/KltPageOrder.vue
@@ -2,8 +2,8 @@
               <div class="middle" style="background: #EEEEEE">
                 <header-bar></header-bar>
                 <div class="middleViewA" style="height: 1.40rem;">
-                   <img src="../../static/image/order/fenlei_1.png"/>
-                  <span>热门分类</span>
+                   <img style="margin-right: 0.1rem;width: 0.4rem" src="../../static/image/order/fenlei_1.png"/>
+                  <span style="font-size: 0.312rem">热门分类</span>
                 </div>
               </div>
 </template>
