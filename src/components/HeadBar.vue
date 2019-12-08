a98e636a91962be6d2d624a7561a78f80c97ca24
studymani <624921088@qq.com>
2019-12-08 13:27:14 +0800

    上传

--- src/components/HeadBar.vue
+++ src/components/HeadBar.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="middleView" style="height: 1.2rem;z-index: 1;background: #FFFFFF">
+  <div class="middleView" style="height: 1.2rem;z-index: 1;background: #FFFFFF;border-bottom: 1px solid #e8e8e8;">
     <div class="middleViewAS" style="width: 15%">
       <img class="middleView" src="../../static/image/public/klt_bak_arrow.png" style="width: 0.25rem;height: 0.422rem;margin-left: 0.422rem;"/>
     </div>
     <div class="middleViewA" style="width: 70%;">
-      <span style="font-size: 0.375rem">商品分类</span>
+      <span style="font-size: 0.375rem" v-text="title"></span>
     </div>
     <div class="middleViewAE" style="width: 15%">
       <img src="../../static/image/order/kefu.png" style="width: 0.406rem;height: 0.391rem;margin-right: 0.422rem;"/>
@@ -18,6 +18,10 @@
             return {
 
             }
+        },
+        props: {
+            title: String,
+            required: true
         }
     }
 </script>
---
+++ src/pages/login.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="middle">
+      <header-bar :title="title"></header-bar>
+    </div>
+</template>
+
+<script>
+    import header from '../components/HeadBar.vue'
+    import '../../static/css/common.css' /*引入公共样式*/
+    export default {
+        name: "login",
+        components: {
+            'header-bar': header,
+        },
+        data() {
+            return {
+                title: '会员登录',
+
+            }
+
+        },
+        methods: {
+
+
+        },
+        filters: {
+
+        },
+        watch: {
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
