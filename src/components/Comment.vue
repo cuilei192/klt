5c56df43c8654f2159099284791cc08b87e6e7d0
cuilei192 <876516308@qq.com>
2019-12-11 23:59:46 +0800

    1

---
+++ src/components/Comment.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="v_comment" >
+    <div class="middleView" style="height: 2rem; padding-left: 0.4rem;">
+            <div class="middleViewAS" style="width: 50%">
+              <span v-text="comment_emai"></span><br>
+            </div>
+
+             <div class="middleViewA" style="width: 50%;">
+               <span v-text="comment_time"></span>
+             </div>
+
+            <div class="middleViewS" style="width: 100%;padding-right: 0.4rem;margin-top: 0.4rem;" >
+              <span v-text="comment_message"></span>
+            </div>
+    </div>
+    <div style="width: 100%; height: 1px;background: #e8e8e8;margin: 0.2rem 0"></div>
+  </div>
+
+</template>
+
+<script>
+  import '../../static/css/common.css';
+  export default {
+    data(){
+      return {
+        selected: 'main',
+      }
+    },
+    props: {
+      comment_emai: String,
+      comment_time: String,
+      comment_star:Number,
+      comment_message:String,
+    }
+  }
+</script>
+
+<style>
+
+</style>
--- src/pages/PageGoodsDetail.vue
+++ src/pages/PageGoodsDetail.vue
@@ -4,28 +4,29 @@
 
     <!--顶部导航栏-->
     <div class="middleView" style="height: 1.2rem;z-index: 1;background: #FFFFFF">
-      <div class="middleViewAS" style="width: 13%">
+      <div class="middleViewAS" style="width: 13%;height: 100%">
         <img class="middleView" src="../../static/image/public/klt_bak_arrow.png"
              style="width: 0.3rem;height: 0.5rem;margin-left: 0.422rem;"/>
       </div>
       <!--顶部选项卡-->
-      <div class="middleViewA" style="width: 74%;">
+      <div class="middleViewA" style="width: 74%;;height: 100%">
         <mt-navbar v-model="selected">
           <mt-tab-item id="nav_goods">商品</mt-tab-item>
           <mt-tab-item id="nav_gooodsDetail">详情</mt-tab-item>
           <mt-tab-item id="nav_goods_comment">评论</mt-tab-item>
         </mt-navbar>
       </div>
-      <div class="middleViewAE" style="width: 13%">
+      <div class="middleViewAE" style="width: 13%;height: 100%">
         <img src="../../static/image/order/kefu.png" style="width: 0.5rem;height: 0.5rem;margin-right: 0.422rem"/>
       </div>
     </div>
 
-    <div style="width: 100%; height: 1px;background: #e8e8e8;margin-top: 0.1rem;"></div>
+    <div style="width: 100%; height: 1px;background: #e8e8e8;"></div>
     <!-- tab-container中间页面部分 -->
     <div id="v_container" style="margin-top: 0.5rem">
       <mt-tab-container v-model="selected">
         <mt-tab-container-item id="nav_goods">
+          <div class="middleView">
           <div class="middleViewA" style="padding-left: 0.4rem;margin-bottom: 0.2rem">
             <div class="middleViewS" style="width: 30%;">
               <img :src="goods.img" style="width: 2.4rem;height: 2.4rem;border-radius: 0.188rem;">
@@ -44,8 +45,10 @@
               </div>
             </div>
           </div>
-          <div style="width: 100%; height: 0.45rem;background: #eeeeee;margin-top: 0.1rem;"></div>
 
+
+          <div style="width: 100%; height: 0.45rem;background: #eeeeee;margin-top: 0.1rem;"></div>
+           <!--输入-->
           <div class="middleViewAS" style="height: 1.34rem;border-bottom: 0.02rem solid #E8E8E8;padding-left: 0.5rem;">
             <div class="middleViewAS" style="width: 50%;">充值金额</div>
             <div class="middleViewAS" style="width: 50%"><input type="text" v-model="RechargeMoney" value="1"/></div>
@@ -61,20 +64,28 @@
           <div class="middleViewAS" style="height: 1.34rem;padding-left: 0.5rem;color: #dbdbdb;margin-top: 0.3rem;">
             <span>*应付美金金额(填好人民币数额后自动显示)：<br ><span style="font-size: 0.313rem;color: #ef504a;font-weight: bolder;" v-text="$options.filters.showPrice(goods.limitTimeMoney * RechargeMoney)"></span>(支持所有外币,支付时自动兑)</span>
           </div>
-
-          <div class="middleViewAS" style="height: 2rem;">
+          <!--总计与立即购买-->
+          <div class="middleViewCE" style="height: 7.1rem;">
             <div class="middleViewA" style="width: 60%;height: 1.5rem;background-color: #333333"><span style="font-size: 0.313rem;color: white;font-weight: bolder;">总计： <span  v-text="$options.filters.showPrice(goods.limitTimeMoney * RechargeMoney)"></span></span></div>
             <div class="middleViewA" style="width: 40%;height: 1.5rem;background-color: #22bf64"><span style="color: white">立即购买</span></div>
           </div>
-
+          </div>
         </mt-tab-container-item>
 
-
+        <!--详情-->
         <mt-tab-container-item id="nav_gooodsDetail">
-          <mt-cell v-for="n in 4" :title="'测试 ' + n"/>
+          <div class="middleView" style="padding-left: 0.4rem;padding-right: 0.4rem">
+             <img style="width: 75%" v-for="(value,index) in goods.goods_detail_imgs" :src="value"/>
+          </div>
         </mt-tab-container-item>
+
+        <!--评价-->
         <mt-tab-container-item id="nav_goods_comment">
-          <mt-cell v-for="n in 6" :title="'选项 ' + n"/>
+          <comment v-for="(value,index) in goods_comments" :comment_emai="value.comment_email" :comment_time="value.comment_time" :comment_message="value.comment_message"></comment>
+          <img style="margin-left: 1.5rem;margin-bottom: -0.1rem" src="../../static/image/order/xiangshang.png"/>
+          <div class="middleView" style="">
+            <p style="width: 90%; background: #eeeeee;border-radius: 0.15rem;box-sizing: border-box;padding: 0.18rem"> <span>管理员：您的满意是我们永恒的追求，售后如有任何问题，建议您联系我们，我们将积极妥善地为您解决。</span></p>
+          </div>
         </mt-tab-container-item>
       </mt-tab-container>
     </div>
@@ -85,18 +96,45 @@
 <script>
 
   import '../../static/css/common.css';
+  import comment from '../components/Comment.vue'
   export default {
-    components: {},
+    components: {
+      'comment': comment,
+    },
     data(){
       return {
-        selected: 'main',
+        selected: 'nav_goods_comment',
         RechargeMoney:0.00,
         goods: {
           img: '../../static/image/home/alipay.jpg',
           name: '支付宝红包充值100元',
           onSaleMoney: '15.96',
-          limitTimeMoney: '10.95'
-        }
+          limitTimeMoney: '10.95',
+          goods_detail_imgs:[
+              "../../static/image/home/alipay.jpg",
+              "../../static/image/home/alipay.jpg",
+              "../../static/image/home/alipay.jpg",
+              "../../static/image/home/alipay.jpg",
+              "../../static/image/home/alipay.jpg",
+              "../../static/image/home/alipay.jpg",
+              "../../static/image/home/alipay.jpg",
+          ]
+        },
+
+        goods_comments:[
+          {
+            comment_email:"876516308@qq.com",
+            comment_time: "2019-12-11 23:00:00",
+            comment_star:3,
+            comment_message:"这个东西不错这个东西不错这个东西不错这个东西不错这个东西不错这个东西不错这个东西不错",
+          },
+          {
+            comment_email:"876516308@qq.com",
+            comment_time: "2019-12-11 23:00:00",
+            comment_star:3,
+            comment_message:"这个东西不错这个东西不错这个东西不错这个东西不错这个东西不错这个东西不错这个东西不错",
+          },
+        ]
 
       }
     },
@@ -117,6 +155,14 @@
 
   .mint-navbar {
     width: 80%
+  }
+  .mint-tab-item {
+    padding: 10px 0;
+    font-size: 15px;
+  }
+  .is-selected{
+    border-bottom: 0.1rem solid #21bf64 !important;
+    color: #21bf64 !important;
   }
 
 </style>
